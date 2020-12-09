b2de27d32064f2e94d3cf01d492010c0ecbf95d9
Gavin Liu <du20783@gmail.com>
2020-12-09 17:52:47 +0800

    add product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_094236) do
+ActiveRecord::Schema.define(version: 2020_12_09_094450) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
