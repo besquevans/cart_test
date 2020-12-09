58015552827d2246d4e6733f9efe60e278996b12
Gavin Liu <du20783@gmail.com>
2020-12-09 18:35:39 +0800

    add model cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_102317) do
+ActiveRecord::Schema.define(version: 2020_12_09_103430) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "fav_products", force: :cascade do |t|
     t.integer "user_id"
@@ -42,5 +49,6 @@ ActiveRecord::Schema.define(version: 2020_12_09_102317) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "carts", "users"
   add_foreign_key "orders", "users"
 end
